5691a558eea80abce15604a54b3aed1dfb2195ca
Olabode Ogun <olabodeogun1@gmail.com>
2023-05-17 14:12:54 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,14 @@
+-- 0-privileges
+
+SELECT
+  User, Host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv,
+  Drop_priv, Grant_priv, References_priv, Index_priv, Alter_priv, Create_tmp_table_priv,
+  Lock_tables_priv, Create_view_priv, Show_view_priv, Create_routine_priv,
+  Alter_routine_priv, Execute_priv, Event_priv, Trigger_priv, Reload_priv,
+  Shutdown_priv, Process_priv, File_priv, Grant_option, Super_priv, Create_user_priv,
+  Create_tablespace_priv
+FROM
+  mysql.user
+WHERE
+  User IN ('user_0d_1', 'user_0d_2')
+  AND Host = 'localhost';
