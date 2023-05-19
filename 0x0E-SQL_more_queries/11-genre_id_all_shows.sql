54b6bfa1a6e1da182495b307253102c29c6a2dac
Olabode Ogun <olabodeogun1@gmail.com>
2023-05-18 18:36:11 -0700

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- 11-genre_id_all_shows.sql
+
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- 8-cities_of_california_subquery.sql
+
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
